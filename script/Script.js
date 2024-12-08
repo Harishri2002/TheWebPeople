d6878da257730badf04a1e603937254f0dfc512f
Harishri2002 <harishribr4@gmail.com>
2024-12-08 23:14:57 +0530

    Last_Update

--- script/Script.js
+++ script/Script.js
@@ -1,5 +1,21 @@
-// Toggle the visibility of the navigation menu
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
+//Js code for hamburger menu
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+  const navItems = document.querySelectorAll('.nav-links li a');
+  hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
-  }
+  });
+  navItems.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href').slice(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+      navLinks.classList.remove('show');
+    });
+  });
+});
